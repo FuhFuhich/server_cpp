c44e3e0a5ffa4988e84463149fabc264cef1b250
FuhFuh <159549845+FuhFuhich@users.noreply.github.com>
2025-01-13 23:06:28 +0300

    upd

---
+++ app/app.cpp
@@ -0,0 +1,170 @@
+﻿#include <boost/beast.hpp>
+#include <boost/asio.hpp>
+#include <boost/process.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/algorithm/string.hpp>
+#include <thread>
+#include <fstream>
+#include <sstream>
+#include <atomic>
+#include <iostream>
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace asio = boost::asio;
+namespace process = boost::process;
+namespace filesystem = boost::filesystem;
+
+using tcp = asio::ip::tcp;
+
+std::atomic<bool> server_active(false);
+std::unique_ptr<process::child> server_process = nullptr;
+std::string project_path = filesystem::current_path().string();
+std::string output_file_path = "server_output.txt";
+
+void start_server()
+{
+	if (!server_active)
+	{
+		server_active = true;
+		server_process = std::make_unique<process::child>(
+			process::search_path("server_cpp"), // Исполняемый файл
+			process::std_out > output_file_path,
+			process::std_err > output_file_path,
+			process::start_dir = project_path
+		);
+	}
+}
+
+void stop_server()
+{
+	if (server_active && server_process && server_process->running())
+	{
+		server_process->terminate();
+		server_active = false;
+	}
+}
+
+bool has_changes()
+{
+	process::ipstream fetch_output;
+	process::system("git fetch", process::std_out > fetch_output);
+	process::ipstream local_output, remote_output;
+	std::ostringstream local_head, remote_head;
+
+	process::system("git rev-parse HEAD", process::std_out > local_output);
+	process::system("git rev-parse FETCH_HEAD", process::std_out > remote_output);
+
+	// Чтение вывода из потоков
+	local_head << local_output.rdbuf();
+	remote_head << remote_output.rdbuf();
+
+	return local_head.str() != remote_head.str();
+}
+
+void pull_and_restart()
+{
+	if (has_changes())
+	{
+		stop_server();
+		process::system("git reset --hard");
+		process::system("git clean -fd");
+		process::system("git pull --force");
+		start_server();
+	}
+}
+
+std::string read_logs()
+{
+	std::ifstream log_file(output_file_path);
+	std::ostringstream buffer;
+	if (log_file.is_open())
+	{
+		buffer << log_file.rdbuf();
+	}
+	return buffer.str();
+}
+
+void handle_request(http::request<http::string_body>& req, http::response<http::string_body>& res)
+{
+	if (req.target() == "/turn_on")
+	{
+		pull_and_restart();
+		start_server();
+		res.result(http::status::ok);
+		res.body() = "Server turned on.";
+	}
+	else if (req.target() == "/turn_off")
+	{
+		stop_server();
+		res.result(http::status::ok);
+		res.body() = "Server turned off.";
+	}
+	else if (req.target() == "/get_logs")
+	{
+		res.result(http::status::ok);
+		res.body() = read_logs();
+	}
+	else if (req.target() == "/get_status")
+	{
+		res.result(http::status::ok);
+		res.body() = server_active ? "Active" : "Inactive";
+	}
+	else
+	{
+		res.result(http::status::not_found);
+		res.body() = "Not Found";
+	}
+	res.prepare_payload();
+}
+
+void server_thread(asio::io_context& ioc, unsigned short port)
+{
+	tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), port));
+	while (true)
+	{
+		tcp::socket socket(ioc);
+		acceptor.accept(socket);
+
+		beast::flat_buffer buffer;
+		http::request<http::string_body> req;
+		http::response<http::string_body> res;
+		beast::error_code ec;
+
+		http::read(socket, buffer, req, ec);
+		if (!ec)
+		{
+			handle_request(req, res);
+			http::write(socket, res, ec);
+		}
+	}
+}
+
+int main()
+{
+	const unsigned short port = 5400;
+
+	std::ofstream(output_file_path).close();
+
+	asio::io_context ioc;
+	std::thread server([&]() {
+		server_thread(ioc, port);
+		});
+
+	while (true)
+	{
+		try
+		{
+			pull_and_restart();
+			std::cout << "Pull and restart successful." << std::endl;
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << "Error: " << e.what() << std::endl;
+		}
+
+		std::this_thread::sleep_for(std::chrono::minutes(5));
+	}
+
+	server.join();
+}
---
+++ server/server.cpp
@@ -0,0 +1,126 @@
+﻿#include <boost/asio.hpp>
+
+#include <iostream>
+
+using boost::asio::ip::tcp;
+
+class Server
+{
+private:
+	tcp::acceptor acceptor_;
+
+public:
+	Server(boost::asio::io_context& io_context, short port)
+		: acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+	{
+		try
+		{
+			start_accept();
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << "Exception in SERVER constructor: " << e.what() << "\n";
+		}
+	}
+
+private:
+	void start_accept()
+	{
+		try
+		{
+			std::cout << "start_accept()...\n";
+			auto socket = std::make_shared<tcp::socket>(acceptor_.get_executor());
+
+			acceptor_.async_accept(*socket,
+				[this, socket](boost::system::error_code ec)
+				{
+					if (!ec)
+					{
+						std::cout << "Client is connected\n";
+						start_read(socket);
+					}
+					else
+					{
+						std::cerr << "Error accepting client: " << ec.message() << "\n";
+					}
+
+					start_accept();
+				});
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << "Exception in SERVER start_accept: " << e.what() << "\n";
+		}
+	}
+
+	void start_read(std::shared_ptr<tcp::socket> socket)
+	{
+		try
+		{
+			auto buffer = std::make_shared<std::array<char, 1024>>(); // буффер в 1024*8 бита, т.е. 1024 символа
+
+			socket->async_read_some(boost::asio::buffer(*buffer),
+				[this, socket, buffer](boost::system::error_code ec, std::size_t length) // Указываем на текущий объект для того, чтобы был доступ к классу и его объектам Server
+				{
+					if (!ec)
+					{
+						std::cout << "Received: " << std::string(buffer->data(), length) << "\n";
+						send_message(socket, std::string(buffer->data(), length));
+						this->start_read(socket); // Зацикливаем прослушивание, т.е. ждем следующее сообщение
+
+					}
+					else
+					{
+						std::cout << "Client disconnect\n";
+						socket->close(); // Закрываем сокет, если клиент отключился
+					}
+				});
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << "Exception in SERVER start_read: " << e.what() << "\n";
+		}
+	}
+
+	void send_message(std::shared_ptr<tcp::socket> socket, const std::string& message)
+	{
+		try
+		{
+			auto buffer = std::make_shared<std::string>(message);
+
+			boost::asio::async_write(
+				*socket,
+				boost::asio::buffer(*buffer),
+				[socket, buffer](boost::system::error_code ec, std::size_t)
+				{
+					if (!ec)
+					{
+						std::cout << "Message sent to client: " << *buffer << "\n";
+					}
+					else
+					{
+						std::cerr << "Failed to send message: " << ec.message() << "\n";
+					}
+				});
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << "Exception in SERVER send_message: " << e.what() << "\n";
+		}
+	}
+};
+
+int main()
+{
+	try
+	{
+		setlocale(0, "");
+		boost::asio::io_context io_context;
+		Server server(io_context, 8080);
+		io_context.run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Exception in MAIN: " << e.what() << "\n";
+	}
+}
