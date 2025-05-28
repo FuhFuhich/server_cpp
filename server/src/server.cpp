772c7ebe3b3388ddcdff26388ab60b00534537d0
FuhFuh <159549845+FuhFuhich@users.noreply.github.com>
2025-05-28 19:50:55 +0300

    upd

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -7,7 +7,7 @@ int main()
 		setlocale(0, "");
 
 		boost::asio::io_context io_context;
-		short port = 5401;
+		short port = 5405;
 		Lobby Lobby(io_context, port);
 
 		std::vector<std::thread> threads;
